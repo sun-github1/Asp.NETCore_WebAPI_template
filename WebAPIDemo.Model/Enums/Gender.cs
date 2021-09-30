45c7e6d422e52e0a0898dee6d3b57f83329fc899
sun-github1 <83362853+sun-github1@users.noreply.github.com>
2021-09-30 09:12:58 +0400

    MOdel project added

---
+++ WebAPIDemo.Model/Enums/Gender.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebAPIDemo.Model.Enums
+{
+    public enum Gender
+    {
+        Male,
+        Female,
+        Other
+    }
+}
---
+++ WebAPIDemo.Model/Models/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using WebAPIDemo.Model.Enums;
+
+namespace WebAPIDemo.Model.Models
+{
+    public class Employee
+    {
+        public int EmployeeId { get; set; }
+        public string EmployeeName { get; set; }
+        public int Age { get; set; }
+        public string Email { get; set; }
+        public int DepartmentId { get; set; }
+        public Gender Gender { get; set; }
+    }
+}
