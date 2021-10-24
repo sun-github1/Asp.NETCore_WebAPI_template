ee22a3c681b1e346ee1945e0f030771f47a8cabe
sun-github1 <83362853+sun-github1@users.noreply.github.com>
2021-10-24 18:45:28 +0400

    updated

---
+++ WebAPIDemo.Model/Models/ErrorDetails.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Json;
+
+namespace WebAPIDemo.Model.Models
+{
+    public class ErrorDetails
+    {
+        public int StatusCode { get; set; }
+        public string Message { get; set; }
+        public override string ToString()
+        {
+            return JsonSerializer.Serialize(this);
+        }
+    }
+}
