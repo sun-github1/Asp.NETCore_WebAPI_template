d1f1bbcfe30d491efaead4bd2b0a0c5294633741
sun-github1 <83362853+sun-github1@users.noreply.github.com>
2021-10-24 07:30:18 +0400

    updated using NLog logger

--- WebAPIDemo.API/Startup.cs
+++ WebAPIDemo.API/Startup.cs
@@ -11,6 +11,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 using WebAPIDemo.DAL.Classes;
 using WebAPIDemo.DAL.DBContext;
 using WebAPIDemo.DAL.Interfaces;
@@ -39,6 +40,11 @@ namespace WebAPIDemo.API
             services.AddScoped<IEmployeeRepository, EmployeeRepository>();
 
             services.AddControllers();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPIDemo.API", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -47,6 +53,12 @@ namespace WebAPIDemo.API
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebAPIDemo.API v1"));
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
             }
 
             app.UseHttpsRedirection();
